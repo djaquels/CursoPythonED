cdf4965aad60eb690a1720bbce0e75909bc5fcf2
heptorsj <hjacales@gmail.com>
2018-12-14 13:29:50 -0600

    nuevo curso

---
+++ tipos_python.py
@@ -0,0 +1,7 @@
+# Tuplas
+tupla1 = (1,2,3,4,5) #del mismo tipo
+tupla2 = ('aaa','aabb','abba') #del mismo tipo: cadenas
+tupla3 = (1,'aaaa',True,4.5) #de diferentes tipos
+print(tupla3[2])
+# Listas
+# Diccionarios
