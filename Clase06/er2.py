5521381aea9b050bf275c74490ae9bb44c230b28
heptorsj <hjacales@gmail.com>
2019-01-08 20:54:50 -0600

    Hilos

--- Clase06/er2.py
+++ Clase06/er2.py
@@ -4,3 +4,6 @@ patron = re.compile("[A-Z]\d")
 print(patron.search("sdadasddaA4"))
 if(re.search("[A-Z][0-9].*","A0sadasda")):
     print("id valido")
+
+#para reemplazar
+print(re.sub(r"[a-z]","Simpson","hola"))
